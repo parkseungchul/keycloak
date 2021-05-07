9e2ab7684df2adfdb180c93081fc48ed47c4b0d8
simon <parkseungchul@gmail.com>
2021-05-07 17:43:05 +0900

    keycloak

---
+++ src/test/java/com/psc/sample/keycloak/KeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psc.sample.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
